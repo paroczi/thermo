fc7c51f61a829f1d38d2cc0fde324c45811d90ac
Paróczi Gergő <paroczi@gmail.com>
2017-09-26 22:15:15 +0200

    init nano serial

--- README.md
+++ README.md
@@ -1,8 +1,32 @@
-Ez az otthoni hőmérséklet mérő IoT
 
-todos:
-- install: express, mongoose
+**Sketch for home thermometer service**
+-
+This configuration is monitoring the temperature and humidity with DHT11 sensor on Arduino Nano, and sending the captured data on Serial 9600.
 
-Installation:
-# npm i
-# npm start
+This stream is reading from a small Node.js application, and stored in a Mongo DB.
+
+The stored data can access through a REST endpoint: http://localhost:3002/list in JSON format.
+
+**Requirements:**
+-
+- Arduino Nano
+- DHT11 temperature sensor
+- MongoDB server
+- RoboMongo database client (optional)
+- nodemom (optional: npm install -g nodemon)
+- Node.js server
+
+
+**Arduino Nano config:**
+- wiring: attached *dht11_wiring.png*
+- DHTtster_copy.ino file
+    * required library: https://github.com/adafruit/DHT-sensor-library
+- from Computer via USB:
+    * select the connected port (e.g. COM3 -> DHTPIN 3 )
+
+**MongoDB config**
+- run "mongod" in the background (first add C:/data/db folder)
+
+**Start**
+- run "yarn install" or "npm install"
+- run "node app.js" or "nodemon app.js" (if you want to automatically restart the app on any change)
--- app.js
+++ app.js
@@ -1,70 +1,49 @@
+var SerialPort = require("serialport");
+const Readline = SerialPort.parsers.Readline;
+var port = new SerialPort('COM3', {
+    baudRate: 9600
+});
+const parser = new Readline();
+port.pipe(parser);
 var express = require('express');
 var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/test', function (error) {
-    if (error) {
-        console.log(error);
-    }
-});
-
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log("sikerult csatlakozni a dbhez")
-});
-
+mongoose.connection.openUri('mongodb://localhost/home', {/* options */});
 
 var thermoSchema = mongoose.Schema({
     time: String,
     temperature: Number,
-    humidity: Number
+    humidity: Number,
+    heat_index: Number
 });
-
 // schema into model
-var SensorDatas = mongoose.model('thermos', thermoSchema);
+var SensorDatas = mongoose.model('thermo', thermoSchema);
 
 var app = express();
 
 
-/**
- * Let's creat the .tpl and .error on the res object
- */
-app.use(function (req, res, next) {
-  res.tpl = {};
-  res.tpl.error = [];
-
-  return next();
-})
+//data event listener
+parser.on('data', function (data) {
+    var item = new SensorDatas();
+    item.time = new Date();
 
 
+    item = Object.assign(item, JSON.parse(data));
+    item.save(err => {
+        if (err) {
+            console.log(err)
+        }
+    });
+});
 
+//rest api
+app.get('/list', function (req, res) {
 
-app.get('/', function(req, res){
-
-  SensorDatas.find({}, '-_id', function (err, docs) {
-        console.log(json(docs))
-    }).sort({_id:-1}).limit(10)
-
-//todo
-
-})
-
-
-
-
-/**
- * Standard error handler
- */
-app.use(function (err, req, res, next) {
-  res.status(500).send('Houston, we have a problem!');
-
-  //Flush out the stack to the console
-  console.error(err.stack);
+    SensorDatas.find({}, '-_id', function (err, docs) {
+        res.json(docs);
+    }).sort({_id: -1}).limit(100);
 });
 
 
-
 var server = app.listen(3002, function () {
-  console.log('Hello on 3002');
+    console.log('Hello on 3002');
 });
